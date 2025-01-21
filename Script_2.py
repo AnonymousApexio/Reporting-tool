7d8ea7821f857b19b6570bf906479be8cd02018e
Apex <149327582+AnonymousApexio@users.noreply.github.com>
2025-01-21 11:39:17 +0100

    Création du modèle du fichier script_2

---
+++ Script_2.py
@@ -0,0 +1,25 @@
+### Script numéro 2
+
+### Partie numéro 1:
+import ...
+
+
+### Partie numéro 2:
+def build_list(repertoire_de_base):
+  ...
+
+### Partie numéro 3:
+def sort_function(liste_fichiers):
+  ...
+
+### Partie numéro 4:
+def filter_function(liste_fichiers, TAILLE_MINI_FICHIER_EN_MEGA_OCTET_ NB_MAXI_FICHIERS):
+  ...
+
+### Partie numéro 5:
+def build_json(liste_fichiers, json_name):
+  ...
+
+### Partie numéro 6:
+if __name__==__main__:
+  ...
