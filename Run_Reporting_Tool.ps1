7e8adf5ed9461f8ff1aaf08c05d512b1e0ab47a1
Apex <149327582+AnonymousApexio@users.noreply.github.com>
2025-02-01 20:06:01 +0100

    Update Run_Reporting_Tool.ps1

--- Run_Reporting_Tool.ps1
+++ Run_Reporting_Tool.ps1
@@ -1,12 +1,14 @@
-$rep_base = python .\Script_1.py
+$OutputEncoding = [System.Text.Encoding]::UTF16
+
+$rep_base = (python .\Script_1.py)
 if (-not $rep_base) {
     Write-Host "Aucun répertoire sélectionné."
     exit
 }
 
 Write-Host "Directory passed to Script_2: $rep_base"
-
 python .\Script_2.py $rep_base
+
 if (-not (Test-Path "gros_fichiers.json")) {
     Write-Host "Erreur lors de l'analyse du répertoire."
     exit
