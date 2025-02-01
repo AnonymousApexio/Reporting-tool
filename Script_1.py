38e148a32bcddb5d772fc3940abd86e3459c1f6c
Apex <149327582+AnonymousApexio@users.noreply.github.com>
2025-02-01 18:52:11 +0100

    Création du Script_1.py

--- Script_1.py
+++ Script_1.py
@@ -1,1 +1,36 @@
+### Script numéro 1
 
+### Partie 1:
+import sys
+from pathlib import Path
+from PyQt5.QtWidgets import QApplication, QFileDialog, QWidget
+
+### Partie 2:
+def choisir_repertoire() -> Path | None:
+    """
+    Ouvre une boîte de dialogue pour sélectionner un répertoire
+    Nous donne le chemin sélectionné sous forme d’objet "Path", ou None si annuléer
+    """
+    app = QApplication(sys.argv)
+    widget = QWidget()
+    widget.hide()  # Empêche l’affichage d’une fenêtre superflue
+    
+    dossier = QFileDialog.getExistingDirectory(widget, "Sélectionnez un répertoire", str(Path.home()))
+    
+    return Path(dossier) if dossier else None
+
+### Partie 3:
+def main():
+    # Sélection du répertoire
+    repertoire_de_base = choisir_repertoire()
+    
+    # Validation et affichage
+    if repertoire_de_base:
+        print(f"Répertoire sélectionné : {repertoire_de_base}")
+    else:
+        print("Aucun répertoire sélectionné.")
+    
+    sys.exit()
+
+if __name__ == '__main__':
+    main()
